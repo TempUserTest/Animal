83f3be74798e8f34f9f74d4cf400b3b1fe100846
TempUserTest <42793236+TempUserTest@users.noreply.github.com>
2018-08-29 14:41:49 +0800

    Add throws UnSupportException

--- Bird.java
+++ Bird.java
@@ -2,7 +2,7 @@ class Bird extends Animal {
 	void fly() throws UnSupportException {
 		System.out.println("I am flying");
 	}
-	void sing() {
+	void sing() throws UnSupportException {
 		System.out.println("I am singing");
 	}
 }
