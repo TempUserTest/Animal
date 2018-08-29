28b9cf6d28e5834d0a1819eaf889ee787c0ef051
TempUserTest <42793236+TempUserTest@users.noreply.github.com>
2018-08-29 15:04:57 +0800

    Create Clownfish.java

---
+++ Clownfish.java
@@ -0,0 +1,12 @@
+class Clownfish extends Fish {	
+	void size () {
+		System.out.println("I am small");
+	}
+	void color () {
+		System.out.println("I am colourful");
+	}
+	void characters()
+	{
+		System.out.println("I make jokes");
+	}
+}
