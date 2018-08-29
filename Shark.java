15f034e8a473fe550db42a66806f78f7b48be31d
TempUserTest <42793236+TempUserTest@users.noreply.github.com>
2018-08-29 15:04:32 +0800

    Create Shark.java

---
+++ Shark.java
@@ -0,0 +1,12 @@
+class Shark extends Fish {	
+	void size () {
+		System.out.println("I am large");
+	}
+	void color () {
+		System.out.println("I am grey");
+	}
+	void characters()
+	{
+		System.out.println("I eat other fish");
+	}
+}
