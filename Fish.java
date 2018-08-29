51dfb4759f6c00e76995ac84b05ec50ce4a018dd
TempUserTest <42793236+TempUserTest@users.noreply.github.com>
2018-08-29 14:42:31 +0800

    Create Fish.java

---
+++ Fish.java
@@ -0,0 +1,11 @@
+class Fish extends Bird {
+	void swim() {
+		System.out.println("I am swimming");
+	}
+	void sing() throws UnSupportException {
+		throw new UnSupportException();
+	}
+	void walk() throws UnSupportException {
+		throw new UnSupportException();
+	}
+}
